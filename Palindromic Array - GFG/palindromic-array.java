8df10456fdb95f20f26ccf9b494000d3fde701bb
Arman Hussain <88603433+ArmanHussain786@users.noreply.github.com>
2022-12-01 18:33:21 +0530

    Added solution - LeetHub

---
+++ Palindromic Array - GFG/palindromic-array.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.util.*;
+class PalindromicArray{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for(int i = 0 ;i < n; i++)
+				a[i]=sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.palinArray(a , n));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG
+{
+    public static int plaindrome(int x)
+    {
+        int reverse=0;
+        int t=x;
+        while(x!=0)
+        {
+            int k=x%10;
+            reverse=reverse*10+k;
+            x=x/10;
+        }
+        if(reverse==t)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        
+    }
+	public static int palinArray(int[] a, int n)
+           {
+                  //add code here.
+                  for(int i=0;i<n;i++)
+                  {
+                      if(plaindrome(a[i])==0)
+                      {
+                          return 0;
+                      }
+                  }
+                  return 1;
+           }
+}
