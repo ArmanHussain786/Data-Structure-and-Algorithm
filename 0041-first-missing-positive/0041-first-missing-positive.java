1286bd803b541b03ca7f1524f57ca4e468850f2b
Arman Hussain <88603433+ArmanHussain786@users.noreply.github.com>
2023-01-08 15:06:24 +0530

    Time: 12 ms (25.75%), Space: 50.5 MB (86.70%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        if(n==1)
+        {
+            if(nums[0]==1)
+            {
+                return 2;
+            }
+            if(nums[0]<=0)
+            {
+                return 1;
+            }
+        }
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+        for(int i=1;i<=n+1;i++)
+        {
+            if(!mp.containsKey(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
