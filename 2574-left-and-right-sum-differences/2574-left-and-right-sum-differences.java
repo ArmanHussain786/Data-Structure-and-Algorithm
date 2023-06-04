16567550a30297efddcd5ec6a7d28abc8afa22ed
Arman Hussain <88603433+ArmanHussain786@users.noreply.github.com>
2023-06-04 10:25:25 +0530

    Time: 1 ms (99.90%), Space: 43.8 MB (17.76%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,25 +1,42 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        int ans =0;
-        int prefix[]= new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-            prefix[i]=ans;
-            ans = ans+nums[i];
+//         int ans =0;
+//         int prefix[]= new int[nums.length];
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             prefix[i]=ans;
+//             ans = ans+nums[i];
            
+//         }
+//         int []suffix = new int[nums.length];
+//         int res =0;
+//         for(int i=nums.length-1;i>=0;i--)
+//         {
+//             suffix[i]=res;
+//             res = res+nums[i];
+//         }
+//         int result []=new int[nums.length];
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             result[i]=Math.abs(prefix[i]-suffix[i]);
+//         }
+//         return result;
+        int n = 0;
+        int l = nums.length;
+        int[] ans = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            ans[i] = n;
+            n += nums[i];
         }
-        int []suffix = new int[nums.length];
-        int res =0;
-        for(int i=nums.length-1;i>=0;i--)
-        {
-            suffix[i]=res;
-            res = res+nums[i];
+        // return ans;
+        n = 0;
+
+        for (int i = l-1; i >= 0; i--) {
+            ans[i] = Math.abs(ans[i] - n);
+            n += nums[i];
         }
-        int result []=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-            result[i]=Math.abs(prefix[i]-suffix[i]);
-        }
-        return result;
+
+       return ans;
     }
 }
