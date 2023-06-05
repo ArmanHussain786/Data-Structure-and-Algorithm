b7e80325627fb53c8ce54c77bae88262423d19e8
Arman Hussain <88603433+ArmanHussain786@users.noreply.github.com>
2023-06-05 08:15:26 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (17.75%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -1,18 +1,21 @@
 class Solution {
-    public boolean checkStraightLine(int[][] coordinates) {
-        int[] a = coordinates[0];
-    int[] b = coordinates[1];
-    int x1 = a[0];
-    int x2 = b[0];
-    int y1 = a[1];
-    int y2 = b[1];
+    public boolean checkStraightLine(int[][] c) {
+        int xMove = c[1][0] - c[0][0];
+        int yMove = c[1][1] - c[0][1];
+        
+        for(int i=1; i<c.length; i++){
 
-    for (int i = 2; i < coordinates.length; i++) {
-      int[] curr = coordinates[i];
-      int x3 = curr[0];
-      int y3 = curr[1];
-      if((y2 - y1)*(x3 - x1) != (x2 - x1)*(y3 - y1)) return false;
-    }
-    return true;
+            int x = c[i][0] - c[i-1][0];
+            int y = c[i][1] - c[i-1][1];
+            
+// linear function -> y = k * x + b;
+// Here need to discuss y = k * x;
+// k = yMove / xMove;
+// y * xMove = x * yMove;
+            
+            if(y * xMove != x * yMove) return false;
+        }
+        
+        return true;
     }
 }
